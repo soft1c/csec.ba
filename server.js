fb7c77d8aa724d68335af166ba596215205814b7
soft1c <softicahmedd@gmail.com>
2024-02-25 11:14:10 +0100

    htmlovi

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const express = require ('express')
-const app = express()
-const port = 3000
+const express = require ('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/login',(req,res)=>{
+    res.sendFile('login.html',{root:__dirname});
+})
+
+app.get('/tehnicar',(req,res)=>{
+    res.sendFile('tehnicar.html',{root:__dirname});
+})
+
+app.get('/ljekar',(req,res)=>{
+    res.sendFile('ljekar.html',{root:__dirname});
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
