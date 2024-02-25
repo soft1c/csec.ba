bc6b8c30664ab58b267c9195143b6291100b25e4
soft1c <softicahmedd@gmail.com>
2024-02-25 12:37:11 +0100

    login poboljsan

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require ('express');
 const mysql= require('mysql');
 const app = express();
 const port = 3000;
+const session = require('express-session');
 
+app.use(session({
+  secret: 'your_secret_key', // Use a secret key for session encryption
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: !true } // Set secure to true if you are using https
+}));
 
 app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: true })); // For URL-encoded data
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
 
 const connection = mysql.createConnection({
@@ -39,38 +46,49 @@ app.get('/', (req, res) => {
     res.sendFile('login.html', {root: './public'});
 });
 
-app.post('/login',(req,res)=>{
-    console.log(req);
+app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     
-    let query=`SELECT * FROM ljekar WHERE username = '${username}' AND password = '${password}'`;
+    let query = `SELECT * FROM ljekar WHERE username = '${username}' AND password = '${password}'`;
     connection.query(query, (error, results, fields) => {
         if (error) throw error;
-        if(results.length>0){
+        if (results.length > 0) {
+            req.session.role = 'ljekar';
             res.sendFile('ljekar.html', {root: './public'});
-        }else{
-            
-            let quey2=`SELECT * FROM tehnicar WHERE username = '${username}' AND password = '${password}'`;
-            connection.query(quey2, (error, results, fields) => {
+        } else {
+            let query2 = `SELECT * FROM tehnicar WHERE username = '${username}' AND password = '${password}'`;
+            connection.query(query2, (error, results, fields) => {
                 if (error) throw error;
-                if(results.length>0){
+                if (results.length > 0) {
+                    req.session.role = 'tehnicar';
                     res.sendFile('tehnicar.html', {root: './public'});
-                }else{
+                } else {
                     res.sendFile('login.html', {root: './public'});
                 }
             });
         }
     });
-})
-
-app.get('/tehnicar', (req, res) => {
-    res.sendFile('tehnicar.html', {root: './public'});
 });
 
-app.get('/ljekar', (req, res) => {
-    res.sendFile('ljekar.html', {root: './public'});
-});
+function checkRole(role) {
+    return function(req, res, next) {
+      if (req.session.role && req.session.role === role) {
+        next();
+      } else {
+        res.sendFile('login.html', {root: './public'});
+      }
+    }
+  }
+  
+  
+  app.get('/ljekar', checkRole('ljekar'), (req, res) => {
+      res.sendFile('ljekar.html', {root: './public'});
+  });
+  
+  app.get('/tehnicar', checkRole('tehnicar'), (req, res) => {
+      res.sendFile('tehnicar.html', {root: './public'});
+  });
 
 
 
