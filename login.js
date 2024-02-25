433383237a36fb7d89b6f9624be81d9514da05f2
goralija <hgoralja2@etf.unsa.ba>
2024-02-25 11:45:16 +0100

    login stranica

---
+++ login.js
@@ -0,0 +1,40 @@
+//=================SHOW MENU
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle')
+      navClose = document.getElementById('nav-close')
+
+navToggle.addEventListener('click', ()=> {
+    navMenu.classList.add('show-menu')
+})
+
+navClose.addEventListener('click', ()=> {
+    navMenu.classList.remove('show-menu')
+})
+
+
+//====================SEARCH
+const search = document.getElementById('search'),
+      searchBtn = document.getElementById('search-btn'),
+      searchClose = document.getElementById('search-close')
+
+searchBtn.addEventListener('click', ()=> {
+    search.classList.add('show-search')
+})
+
+searchClose.addEventListener('click', ()=> {
+    search.classList.remove('show-search')
+})
+
+
+//====================LOGIN
+const login = document.getElementById('login'),
+      loginBtn = document.getElementById('login-btn'),
+      loginClose = document.getElementById('login-close')
+
+loginBtn.addEventListener('click',()=>{
+    login.classList.add('show-login')
+})
+
+loginClose.addEventListener('click',()=>{
+    login.classList.remove('show-login')
+})
